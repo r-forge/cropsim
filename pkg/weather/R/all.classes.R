058cb07c8900b1797c80ef2b4f9c0f1b40bcbc57
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-01-22 00:42:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropsim@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/weather/R/all.classes.R
@@ -0,0 +1,52 @@
+# R classes for spatial data (raster data specifically) 
+# Author: Robert J. Hijmans
+# International Rice Research Institute
+# contact: r.hijmans@gmail.com
+# Date : October 2008
+# Version 0,3
+# Licence GPL v3
+
+
+setClass('location', 
+	representation (
+		x ='numeric',
+		y ='numeric',		
+		alt ='numeric'
+	)
+)
+
+
+setClass('weather', 
+	representation (
+		wth ="data.frame"
+		temp ='vector',
+		prec = 'vector',
+		rad = 'vector',
+		wind = 'vector',
+		vapr = 'vector',
+		relh  = 'vector',
+		time = 'vector',
+	),
+	prototype (	
+		
+	),	
+	validity = function(object)
+	{ # all vectors should have same length }
+)
+
+	
+setClass ('dailyweather',
+	contains = 'weather',
+	representation (
+		mintmp ='numeric',
+		maxtmp ='numeric'
+		),
+	validity = function(object)
+	{
+		cond1 <- isTRUE(object@mintmp <= object@maxtmp)
+		cond <- cond1 #& cond2 & cond3
+		return(cond)
+	}
+)
+	
+	
