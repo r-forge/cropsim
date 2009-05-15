eb78cb1280ee46fd98af9d37dd5c99aebd3e53f7
ipangga (Dr ) <I.Pangga@cgiar.org>
2009-05-15 01:49:08 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropsim@60 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/cropsim/R/sheathblight.R
+++ pkg/cropsim/R/sheathblight.R
@@ -61,8 +61,8 @@ sheathBlight <- function(wth, emergence='2000-05-15', onset=30, duration=120, rh
 	RSenesced <- vector(length=duration)
 	RSenesced[] <- 0
 	
-	Incidence <- vector (length=duration)
-	Incidence[] <- 0
+	Severity <- vector (length=duration)
+	Severity[] <- 0
 
 	# Boxcar
 	infectious <- vector(length=duration)
@@ -155,14 +155,14 @@ sheathBlight <- function(wth, emergence='2000-05-15', onset=30, duration=120, rh
 	#sday <- max(1, sday)
 	#now_Sites[day] <- sum(Sites[sday:day])
 		
-		Incidence[day] <- (Diseased[day]-Removed[day])/(TotalSites[day] - Senesced[day] - Removed[day])*100
+		Severity[day] <- (Diseased[day]-Removed[day])/(TotalSites[day] - Senesced[day] - Removed[day])*100
 	}
 	
-	res <- cbind(Sites, now_latent, now_infectious, Removed, Diseased, Senesced, Rinfection, Rtransfer, RGrowth, RSenesced, Incidence)
+	res <- cbind(Sites, now_latent, now_infectious, Removed, Diseased, Senesced, Rinfection, Rtransfer, RGrowth, RSenesced, Severity)
 	res <- res[1:day,]
 	#res <- Diseased / AllSites 
 	res <- cbind(1:length(res[,1]), res)
-	colnames(res) <- c("day", "sites", "latent", "infectious", "removed", "diseased", "senesced", "rateinf", "rtransfer", "rgrowth", "rsenesced", "incidence")
+	colnames(res) <- c("day", "sites", "latent", "infectious", "removed", "diseased", "senesced", "rateinf", "rtransfer", "rgrowth", "rsenesced", "severity")
 	return(res)
 }
 
