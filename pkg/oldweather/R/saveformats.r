62f436664cd10ba9d84e29c4bd4923eb7043d696
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-08-27 04:31:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropsim@143 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/oldweather/R/saveformats.r
+++
@@ -1,8 +0,0 @@
-# Author: Jorrel Khalil S. Aunario, jaunario@gmail.com
-# Date :  22 September 2010
-# Version 0.0.1
-# Licence GPL v3
-
-write.fse <- function(wth, year, country, stn, author="", src="", target=".", na.val=-99, y2ksafe=1, sradkJ=TRUE, ...){    
-stop('see function writeCABOwth')
-}
