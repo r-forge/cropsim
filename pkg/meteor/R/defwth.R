4dccf844c452a165427f1224883bdd5859df8168
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2017-12-12 00:39:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropsim@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/meteor/R/defwth.R
+++ pkg/meteor/R/defwth.R
@@ -2,7 +2,7 @@
 # License GPL3
 # Version 1.0  December 2012
 
-default_weather <- function() {
+example_weather <- function() {
 	f <- system.file("extdata/18419.rds", package="meteor")
 	readRDS(f)
 }
