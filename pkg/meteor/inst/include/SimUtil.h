1c2a54be06135054d8dba7b7348959b1c261e29e
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2017-12-12 04:18:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/cropsim@203 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/meteor/inst/include/SimUtil.h
+++ pkg/meteor/inst/include/SimUtil.h
@@ -12,7 +12,7 @@
 
 class DailyWeather {
 	public:
-		double longitude, latitude, elevation, DAYLP;
+		double longitude, latitude, elevation, DAYLP, ANGSTA, ANGSTB;
 		double CO2 = 400;
 		std::vector<long> date;
 		std::vector<double> srad, tmin, tmax, prec, wind, vapr;
@@ -50,6 +50,40 @@ inline double INSW(double x, double y1, double y2) {
 inline int REAAND(double x1, double x2) {
 	return( ((x1 > 0) & (x2 > 0)) ? 1 : 0 );
 }	
+
+
+inline double LIMIT(double min, double max, double v) {
+  if (v < min) {
+    v = min;
+  } else if (v > max) {
+    v = max;
+  }
+  return(v);
+}
+
+inline double AFGEN(std::vector<double> xy, double x) {
+  int n = xy.size();
+  if(n == 0){
+    //cout << "input empty matrix for AFGEN" << endl;
+    throw "error";
+    exit(0);
+  }
+  double y = -1;
+  if (x < xy[0]) {
+    y = xy[1];
+  } else if (x > xy[n-2]) {
+    y = xy[n-1];
+  } else {
+    for(int i=2; i<n; i=i+2) {
+      if (xy[i] > x) {
+        double slope = (xy[i+1] - xy[i-1]) / (xy[i] - xy[i-2]);
+        y = xy[i-1] + (x - xy[i-2]) * slope;
+        break;
+      }
+    }
+  }
+  return(y);
+}
 
 
 inline double LINT(std::vector<std::vector<double> > xy, double x) {
